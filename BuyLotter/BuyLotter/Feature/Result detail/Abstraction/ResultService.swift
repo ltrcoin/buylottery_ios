a8ce1cc4d49561f58dff4a15ab4a45ee482cb679
kazypos <kazypos@gmail.com>
2018-10-11 15:37:38 +0700

    Request api
    Change to main Queue thread

--- BuyLotter/BuyLotter/Feature/Result detail/Abstraction/ResultService.swift
+++ BuyLotter/BuyLotter/Feature/Result detail/Abstraction/ResultService.swift
@@ -12,36 +12,31 @@ struct ResultService {
     func getWinningNumbers( completion: @escaping ([PrizeModel]) -> Void ){
         print("😩 getWinningNumbers")
         let urlString = Config.SERVER_LINK + "winningnumbers"
-        
-        let headers: HTTPHeaders = [:]
-        
-        let parameters: Parameters = [:]
+
         var result:[PrizeModel] = []
-        Alamofire.request(urlString, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-            .responseJSON { (response) in
-                if let dataResponse = response.result.value as? [Dictionary<String,Any>] {
-                    for data in dataResponse {
-                        if let gameId = data["game_id"] as? Int, gameId == 3 {
+        Alamofire.request(urlString).responseJSON(completionHandler: { (response) in
+            if let dataResponse = response.result.value as? [Dictionary<String,Any>] {
+                for data in dataResponse {
+                    if let gameId = data["game_id"] as? Int, gameId == 3 {
+                        
+                        
+                        if let numbers = data["numbers"] as? String, let specials = data["special_numbers"] as? String, let dateStr = data["draw_date"] as? String {
+                            var prize = PrizeModel()
+                            let tempN = numbers.split(separator: ",")
+                            let tempS = specials.split(separator: ",")
+                            prize.normalNumbers = tempN.map{Int($0)!}
+                            prize.specialNumbers = tempS.map{Int($0)!}
+                            prize.drawDateStr = dateStr
                             
-                            
-                            if let numbers = data["numbers"] as? String, let specials = data["special_numbers"] as? String, let dateStr = data["draw_date"] as? String {
-                                var prize = PrizeModel()
-                                let tempN = numbers.split(separator: ",")
-                                let tempS = specials.split(separator: ",")
-                                prize.normalNumbers = tempN.map{Int($0)!}
-                                prize.specialNumbers = tempS.map{Int($0)!}
-                                prize.drawDateStr = dateStr
-                                
-                                result.insert(prize, at: 0)
-                            }
-                            
+                            result.insert(prize, at: 0)
                         }
                         
                     }
-                    completion(result)
+                    
                 }
-        }
-        
-        
+                completion(result)
+            }
+        })
+    
     }
 }
